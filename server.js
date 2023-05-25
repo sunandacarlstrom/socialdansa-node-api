545d350cdbe97460aea440720f9292a0093f43c4
Sunanda Carlström <carlstrom-3@hotmail.com>
2023-05-25 11:11:40 +0200

    Testar att servern fungerar med dummy data

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const axios = require("axios");
 
 const app = express();
 
-const PORT = 5020; 
+// Skapar enpointresurser...
+app.get("/api/v1/danceband", async (req, res) => {
+    res.status(200).json({
+        id: 1,
+        name: "Perikles"
+    });
+});
+
+app.post("/api/v1/danceband", async (req, res) => {
+    res.status(201).json({ message: "Sparat uppdatering" });
+});
+
+const PORT = 5020;
 
 app.listen(PORT, () => console.log(`Servern är uppe och kör på port ${PORT}`));
