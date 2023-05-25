34c2d6f41e7123f24c55b57788fe5cd7f74b4a38
Sunanda Carlström <carlstrom-3@hotmail.com>
2023-05-25 11:50:46 +0200

    POST anrop klar

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
+app.use(express.json());
 
 // Skapar enpointresurser...
 app.get("/api/v1/danceband", async (req, res) => {
     const url = "http://localhost:3000/danceband";
-    const {data} = await axios.get(url);
+    const { data } = await axios.get(url);
     res.status(200).json(data);
 });
 
 app.post("/api/v1/danceband", async (req, res) => {
-    res.status(201).json({ message: "Sparat uppdatering" });
+    const url = "http://localhost:3000/danceband";
+    const body = req.body;
+    const { data } = await axios.post(url, body);
+    res.status(201).json(data);
 });
 
 const PORT = 5020;
