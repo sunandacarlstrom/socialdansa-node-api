35b90bbf2442075eba550f2b4b4b87ed3912bcb1
Sunanda Carlström <carlstrom-3@hotmail.com>
2023-05-26 13:24:59 +0200

    Lagt till endpoints. React applikationen Socialdansa konsumerar nu detta API

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
+//tillåter alla att komma åt mina endpoints med cors
+app.use(cors());
 app.use(express.json());
 
 // Skapar enpointresurser...
 
 // listar alla dansband
 app.get("/api/v1/danceband", async (req, res) => {
-    const url = "http://localhost:3000/danceband";
+    const url = "http://localhost:5030/danceband";
     const { data } = await axios.get(url);
     res.status(200).json(data);
 });
@@ -16,14 +19,21 @@ app.get("/api/v1/danceband", async (req, res) => {
 // hämta ett specifikt dansband
 app.get("/api/v1/danceband/:id", async (req, res) => {
     const param = req.params.id;
-    const url = `http://localhost:3000/danceband/${param}`;
+    const url = `http://localhost:5030/danceband/${param}`;
     const { data } = await axios.get(url);
     res.status(200).json(data);
 });
 
-// lägger till ett dansband i listan
-app.post("/api/v1/danceband", async (req, res) => {
-    const url = "http://localhost:3000/danceband";
+// hämta alla meddelanden och visa i Admin
+app.get("/api/v1/messages", async (req, res) => {
+    const url = `http://localhost:5030/message`;
+    const { data } = await axios.get(url);
+    res.status(200).json(data);
+});
+
+// skicka ett meddelande från kontaktformuläret till api
+app.post("/api/v1/messages", async (req, res) => {
+    const url = "http://localhost:5030/message";
     const body = req.body;
     const { data } = await axios.post(url, body);
     res.status(201).json(data);
